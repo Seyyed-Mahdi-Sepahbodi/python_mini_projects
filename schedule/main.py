1ab50c742c2016a499cfb691ef27d8a35ed24b31
Seyyed-Mahdi-Sepahbodi <seyyedmahdisepahbodi@gmail.com>
2022-02-25 07:40:16 -0500

    write every method in Schedule class and write init method of Job class

---
+++ schedule/main.py
@@ -0,0 +1,22 @@
+
+class Scheduler:
+    
+    def __init__(self):
+        self.jobs = []    
+
+    def every(self, interval=1):
+        job = Job(interval)
+        self.jobs.append(job)
+        return job
+
+
+
+class Job:
+    
+    def __init__(self, interval):
+        self.interval = interval
+        self.job_function = None
+        self.last_run = None
+        self.next_run = None
+        self.time_unit = None
+        self.time_period = None    
